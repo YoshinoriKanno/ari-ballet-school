3900b3e5ca14f2b9192f5130bfe156f7f0fd0708
Yoshinori Kanno <yoshinori.kanno@me.com>
2024-04-20 08:39:32 +0900

    ✨ loading 機能

--- dist/scripts/loading.js
+++ dist/scripts/loading.js
@@ -5,6 +5,9 @@ document.addEventListener('touchmove', noscroll, { passive: false });
 document.addEventListener('wheel', noscroll, { passive: false });
 window.onload = function () {
   const spinner = document.getElementById('loading');
+  if (!spinner) {
+    return;
+  }
   spinner.classList.add('loaded');
   document.removeEventListener('touchmove', noscroll);
   document.removeEventListener('wheel', noscroll);
