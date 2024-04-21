d1f8e92da72d9560ecf239e60f3d7f753021d967
Yoshinori Kanno <yoshinori.kanno@me.com>
2024-04-21 18:23:33 +0900

    🚧  ハンバーガーメニュー

---
+++ dist/scripts/drawer.js
@@ -0,0 +1,46 @@
+const drawer = () => {
+  const menuItems = document.querySelectorAll('.js-menu-item');
+  const nav = document.querySelector('.p-nav-full-screen');
+  const openDrawer = document.querySelector('.js-toggle-hamburger');
+
+  function showNav() {
+    if (!nav || !openDrawer) return;
+    nav.classList.add('is-show');
+    document.body.classList.add('is-fixed');
+    openDrawer.classList.add('is-active');
+  }
+
+  function hideNav() {
+    if (!nav || !openDrawer) return;
+    nav.classList.remove('is-show');
+    document.body.classList.remove('is-fixed');
+    openDrawer.classList.remove('is-active');
+  }
+  function toggleNav() {
+    if (!nav || !openDrawer) return;
+
+    const isNavVisible = nav.classList.contains('is-show');
+
+    if (isNavVisible) {
+      hideNav();
+    } else {
+      showNav();
+    }
+  }
+
+  menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      hideNav();
+    });
+  });
+
+  openDrawer.addEventListener('click', () => {
+    toggleNav();
+  });
+  // リサイズ時にナビゲーションを閉じる
+  window.addEventListener('resize', () => {
+    hideNav();
+  });
+};
+
+export default drawer;
--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,3 +1,5 @@
+import drawer from './drawer.js';
 import scrollArrow from './scroll-arrow.js';
 
+drawer();
 scrollArrow();
